4d26d985c0483d0f06568ea7daa6b1c629939ee6
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-04-10 10:07:42 +0200

    Change tests data paths in unit test

--- tests/test_resize_algorithms.py
+++ tests/test_resize_algorithms.py
@@ -4,19 +4,19 @@ from imgprocalgs.algorithms import resize
 #TODO: more tests
 class TestNearestNeighbour(TestCase):
     def test_resize(self):
-        algos = resize.NearestNeigbhour('data/desert.jpg', 2)
+        algos = resize.NearestNeigbhour('tests/data/desert.jpg', 2)
         algos.process()
 
     def test_resize_bird(self):
-        algos = resize.NearestNeigbhour('data/bird.jpg', 4)
+        algos = resize.NearestNeigbhour('tests/data/bird.jpg', 4)
         algos.process()
 
 
 class TestBilinearInterpolation(TestCase):
     def test_resize(self):
-        algos = resize.BilinearInterpolation('data/desert.jpg', 2)
+        algos = resize.BilinearInterpolation('tests/data/desert.jpg', 2)
         algos.process()
 
     def test_resize_bird(self):
-        algos = resize.BilinearInterpolation('data/bird.jpg', 4)
+        algos = resize.BilinearInterpolation('tests/data/bird.jpg', 4)
         algos.process()
