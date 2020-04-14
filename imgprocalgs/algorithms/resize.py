a4086fa443f4fc0589e91cf54d17c1ffa030b627
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-04-14 09:01:13 +0200

    Add utilities module

--- imgprocalgs/algorithms/resize.py
+++ imgprocalgs/algorithms/resize.py
@@ -4,22 +4,8 @@ Testing with Pillow
 """
 import abc
 import math
-
 from PIL import Image as PillowImage
-
-
-class Image:
-    """ Wrapper for Image class for easier usage"""
-    def __init__(self, image_path: str):
-        self.image_path = image_path
-        self.image: PillowImage = PillowImage.open(self.image_path)
-        self.pixels = self.image.load()
-
-    def get_size(self):
-        """
-        :return: x, y in pixels
-        """
-        return self.image.size[0], self.image.size[1]
+from imgprocalgs.utilities import Image
 
 
 class ImageResizer(metaclass=abc.ABCMeta):
---
+++ imgprocalgs/utilities.py
@@ -0,0 +1,16 @@
+""" Module including utilities for main algorithms"""
+from PIL import Image as PillowImage
+
+
+class Image:
+    """ Wrapper for Image class for easier usage"""
+    def __init__(self, image_path: str):
+        self.image_path = image_path
+        self.image: PillowImage = PillowImage.open(self.image_path)
+        self.pixels = self.image.load()
+
+    def get_size(self):
+        """
+        :return: x, y in pixels
+        """
+        return self.image.size[0], self.image.size[1]
