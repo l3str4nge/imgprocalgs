128d353be0d19d2b3d34386d69b1c6dadac0326e
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-04-15 22:01:30 +0200

    Add negative function tests

--- tests/test_negative.py
+++ tests/test_negative.py
@@ -1,9 +1,24 @@
 from unittest import TestCase
 from imgprocalgs.algorithms import negative
+from PIL import Image
 
 
 class TestNegative(TestCase):
+    TEST_IMAGE = "tests/data/test.jpg"
+
+    def setUp(self):
+        self.image = Image.new("RGB", (100, 100), "#FFFFFF")
+        self.image.save(self.TEST_IMAGE)
+
     def test_negative(self):
-        negative.make_negative("tests/data/desert.jpg")
+        """ Create (255, 255, 255) image and  check if all (0, 0, 0)"""
+        negative.make_negative(self.TEST_IMAGE)
+
+        pixels = Image.open('negative.jpg').load()
+        self.assertEqual(pixels[0, 0], (0, 0, 0))
+
+
+
+
 
 
