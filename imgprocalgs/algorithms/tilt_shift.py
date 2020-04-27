ae12dc5b8299e2515b42f4f697b872a0af06be95
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-04-27 16:51:39 +0200

    Generating tilt shift output

--- imgprocalgs/algorithms/tilt_shift.py
+++ imgprocalgs/algorithms/tilt_shift.py
@@ -1,6 +1,7 @@
-from PIL import Image as PillowImage
+import os
 import itertools
 from math import sqrt, pi, e
+from imgprocalgs.algorithms.utilities import create_empty_image
 
 from imgprocalgs.algorithms.utilities import Image
 
@@ -10,7 +11,8 @@ class TiltShift:
         self.image_path = image_path
         self.destination_path = destination_path
         self.input_image = Image(self.image_path)
-        self.filter_size = 0
+        self.pixels = self.input_image.pixels
+        self.filter_elements = []
 
         self.min_factor = 0.003
         self.max_factor = 0.003
@@ -20,7 +22,7 @@ class TiltShift:
 
     @classmethod
     def _make_filter_factor(cls, blur: float, index: int):
-        return (1 / sqrt(2 * pi * blur)) * pow(e, -(pow(index, 2)/2 * pow(blur, 2)))
+        return (1 / (sqrt(2 * pi) * blur)) * pow(e, -(pow(index, 2)/2 * pow(blur, 2)))
 
     def _make_blur(self, distance: int, sharp_area_size: int):
         """
@@ -29,3 +31,38 @@ class TiltShift:
         :return: blur value
         """
         return self.min_blur + (self.max_blur - self.min_blur) * distance / sharp_area_size
+
+    def generate_filter_elements(self, blur: float, max_factor: int):
+        self.filter_elements = list(itertools.takewhile(
+            self.is_gt_min_factor,
+            [self._make_filter_factor(blur, i) for i in range(max_factor)]
+        ))
+
+    def is_gt_min_factor(self, value: float):
+        return value >= self.min_factor
+
+    def process(self):
+        width, height = self.input_image.get_size()
+        output = create_empty_image(width, height)
+        output_pixels = output.load()
+
+        for x in range(width):
+            for y in range(height):
+                blur = self._make_blur(1, 1)  # TODO: add logic, fixed value now
+                self.generate_filter_elements(blur, 7)
+                red = self.process_component(0, x, y)
+                green = self.process_component(1, x, y)
+                blue = self.process_component(2, x, y)
+                output_pixels[x, y] = (red, green, blue)
+
+        output.save(os.path.join(self.destination_path, 'output_tilt_shift.jpg'))
+
+    def process_component(self, index, x, y):
+        numerator = self.filter_elements[0] * self.pixels[x, y][index]
+        denumerator = self.filter_elements[0]
+        print(self.filter_elements)
+        for i, value in enumerate(self.filter_elements[1:]):
+            numerator += value * self.pixels[x - i, y][index] + value * self.pixels[x + 1, y][index]
+            denumerator += 2 * value
+
+        return int(numerator / denumerator)
--- tests/test_tiltshift.py
+++ tests/test_tiltshift.py
@@ -4,20 +4,31 @@ from imgprocalgs.algorithms.tilt_shift import TiltShift
 
 
 class TestTiltShift(TestCase):
-    TEST_IMAGE = "tests/data/desert.jpg"
-    DEST_PATH = "/tests/data"
+    TEST_IMAGE = "tests/data/city.jpg"
+    DEST_PATH = "tests/data"
 
     def test_make_filter_factor(self):
         self.assertEqual(0.24197072451914337, TiltShift._make_filter_factor(1, 1))
-        self.assertEqual(9.463226016607707e-05, TiltShift._make_filter_factor(2, 2))
+        self.assertEqual(6.691511288244271e-05, TiltShift._make_filter_factor(2, 2))
 
     def test_blur(self):
         ts = TiltShift(self.TEST_IMAGE, self.DEST_PATH, 1, 4)
         self.assertEqual(1.075, ts._make_blur(5, 200))
         self.assertEqual(2.8461538461538463, ts._make_blur(8, 13))
 
+    def test_gt_min_factor(self):
+        ts = TiltShift(self.TEST_IMAGE, self.DEST_PATH, 1, 4)
 
+        ts.min_factor = 2
+        self.assertTrue(ts.is_gt_min_factor(10))
+        self.assertFalse(ts.is_gt_min_factor(1))
 
+    def test_generate_filter(self):
+        ts = TiltShift(self.TEST_IMAGE, self.DEST_PATH, 0.1, 6)
 
+        ts.generate_filter_elements(blur=0.5, max_factor=700)
+        self.assertEqual(7, len(ts.filter_elements))
 
-
+    def test_process(self):
+        ts = TiltShift(self.TEST_IMAGE, self.DEST_PATH, 1, 6)
+        ts.process()
