b2bc37c5da50c6117fc579b2a187b5e6fc2563fa
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-05-05 11:37:18 +0200

    Added RGB to HSV conversion

--- imgprocalgs/algorithms/utilities.py
+++ imgprocalgs/algorithms/utilities.py
@@ -3,6 +3,7 @@ from PIL import Image as PillowImage
 from collections import namedtuple
 
 ImageData = namedtuple("ImgData", 'header image')
+HSV = namedtuple("HSV", 'h s v')
 
 
 class Image:
@@ -23,5 +24,33 @@ def create_empty_image(width: int, height: int) -> PillowImage:
     return PillowImage.new("RGB", (width, height), "#000000")
 
 
-def get_greyscale(red, green, blue):
+def get_greyscale(red: int, green: int, blue: int) -> float:
     return 0.2126 * red + 0.587 * green + 0.114 * blue
+
+
+def rgb_to_hsv(red: int, green: int, blue: int) -> tuple:
+    _red = red / 255
+    _green = green / 255
+    _blue = blue / 255
+
+    c_max = max(_red, _green, _blue)
+    c_min = min(_red, _green, _blue)
+
+    delta = c_max - c_min
+
+    if delta > 0:
+        if c_max == _red:
+            h = 60 * (((_green - _blue) % 6) / delta)
+        elif c_max == _green:
+            h = 60 * (((_blue - _red) / delta) + 2)
+        elif c_max == _blue:
+            h = 60 * (((_red - green) / delta) + 2)
+        else:
+            raise ValueError(f"c_max ({c_max} is not equal {_red}/{_green}/{_blue})")
+    else:
+        h = 0
+
+    s = 0 if c_max == 0 else delta/c_max
+
+    return HSV(h, s, c_max)
+
