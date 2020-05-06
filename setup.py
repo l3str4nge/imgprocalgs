d7f35e426248611f1a99c7372c9fe40946f35397
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-05-06 16:16:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="imgprocalgs",
+    version="0.1",
+    author="mateuszz0000",
+    author_email="mtszzwdzk@gmail.com",
+    description="Collection of common image processing algorithms.",
+    long_description="Collection of common image processing algorithms.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mateuszz0000/imgprocalgs",
+    install_requires=required,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
